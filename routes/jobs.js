9aa2262ac03ef16155be3a9fa7a4cec1c6182f2a
Luiz Dantas <dantasluizmanoel@gmail.com>
2024-05-22 11:26:45 -0300

    :sparkles: feat: job details route

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,7 +7,7 @@ const Job = require("../models/Job")
 router.get('/add', (req, res) => {
     res.render('add')
 })
-// add job via post
+// add job
 router.post('/add', (req, res) => {
     let {title, salary, company, description, email, new_job} = req.body;
 
@@ -23,6 +23,18 @@ router.post('/add', (req, res) => {
     .then(()=> res.redirect('/'))
     .catch(err => console.log(err))
 })
+
+//get job detail
+router.get("/view/:id", (req, res) => {
+    Job.findOne({
+        where: {id: req.params.id}
+    }).then(job => {
+        res.render('view',{
+            job
+        })
+    }).catch(err => console.log(err))
+})
+
 
 
 
